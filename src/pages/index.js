679a7aaf546264f6431efeb4880acd51dc78504e
geleeroyale <krassvs@gmail.com>
2020-05-24 23:50:15 +0200

    visual corrections

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,23 @@ import styled from "@emotion/styled"
 import SvgImagemap from "../components/Imagemap"
 import SEO from "../components/seo"
 
+import Logo from "../images/t4h-logo.png"
+
 const Container = styled.div`
   max-height: 100vh;
+  margin: 0;
+
+  .logo {
+    position: absolute;
+    top: 30px;
+    left: 20px;
+  }
 `
 
 const IndexPage = () => (
   <Container>
-    <SEO title="Home" />
+    <SEO title="token4hope" />
+    <img src={Logo} className="logo" hr />
     <SvgImagemap />
   </Container>
 )
