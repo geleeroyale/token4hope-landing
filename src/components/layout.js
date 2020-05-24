9766a318fc7536216293b4c76a459165e9d3b42d
geleeroyale <krassvs@gmail.com>
2020-05-24 21:34:13 +0200

    init

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,14 +30,13 @@ const Layout = ({ children }) => {
         style={{
           margin: `0 auto`,
           maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built by
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://www.collective-energy.at">Collective Energy</a>
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from "@emotion/styled"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+import SvgImagemap from "../components/Imagemap"
 import SEO from "../components/seo"
 
+const Container = styled.div`
+  max-height: 100vh;
+`
+
 const IndexPage = () => (
-  <Layout>
+  <Container>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <SvgImagemap />
+  </Container>
 )
 
 export default IndexPage
